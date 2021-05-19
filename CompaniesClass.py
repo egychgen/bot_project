0a9e3c854a22719322e904429c617700a78f7480
ArtemMint <Artembaobab@gmail.com>
2021-05-19 15:53:10 +0300

    unkhown chenges

--- CompaniesClass.py
+++ CompaniesClass.py
@@ -33,6 +33,12 @@ class Company:
         self.__country = country
 
 
+    def get_tiker(self):
+        return self.__tiker
+
+    def get_country(self):
+        return self.__country
+
     def get_price(self):
         r = requests.get("https://investmint.ru/{0}/".format(self.__tiker), headers={'User-Agent': ch})
         text = r.text
@@ -59,72 +65,89 @@ class Company:
 
 
     def _Get_multiplicator_of_Russian_Comp(self, names):
-        Multiplicator_HTML = requests.get('https://smart-lab.ru/q/{0}/f/y/MSFO'.format(self.__tiker), headers = {'User-Agent': ch } )
-        Multiplicators_Dict = {}
-        text = Multiplicator_HTML.text
-        for i in range(len(names)):
+        try:
+            Multiplicator_HTML = requests.get('https://smart-lab.ru/q/{0}/f/y/MSFO'.format(self.__tiker), headers = {'User-Agent': ch } )
+            Multiplicators_Dict = {}
+            text = Multiplicator_HTML.text
+            for i in range(len(names)):
 
-            div = names[i].split("/")
-            if text.find(div[0] + " / " + div[1]) > 0:
-                Multiplicator_Value = text[text.find(div[0] + " / " + div[1]):]
-            elif text.find(div[0] + "/" + div[1]) > 0:
-                Multiplicator_Value = text[text.find(div[0] + "/" + div[1]):]
-            else:
-                Multiplicators_Dict.update({names[i]: "There is no such multiplicator"})
-                continue
-            Multiplicator_Value = Multiplicator_Value[str(Multiplicator_Value).find("ltm_spc"):]
-            Multiplicator_Value = Multiplicator_Value[str(Multiplicator_Value).find("<td>") + 4:]
-            Multiplicator_Value = str(Multiplicator_Value[:str(Multiplicator_Value).find("</td>")]).split()
-            Value = ""
-            for j in range(len(Multiplicator_Value)):
-                Value = Value + Multiplicator_Value[j]
-            Multiplicators_Dict.update({names[i]: Value})
-        return Multiplicators_Dict
+                div = names[i].split("/")
+                if text.find(div[0] + " / " + div[1]) > 0:
+                    Multiplicator_Value = text[text.find(div[0] + " / " + div[1]):]
+                elif text.find(div[0] + "/" + div[1]) > 0:
+                    Multiplicator_Value = text[text.find(div[0] + "/" + div[1]):]
+                else:
+                    Multiplicators_Dict.update({names[i]: "There is no such multiplicator"})
+                    continue
+                Multiplicator_Value = Multiplicator_Value[str(Multiplicator_Value).find("ltm_spc"):]
+                Multiplicator_Value = Multiplicator_Value[str(Multiplicator_Value).find("<td>") + 4:]
+                Multiplicator_Value = str(Multiplicator_Value[:str(Multiplicator_Value).find("</td>")]).split()
+                Value = ""
+                for j in range(len(Multiplicator_Value)):
+                    Value = Value + Multiplicator_Value[j]
+                    Multiplicators_Dict.update({names[i]: Value})
+        except requests.exceptions.HTTPError as error:
+            print("Request error", error.response.status_code)
+            Multiplicators_Dict = {"No": 0}
+        finally:
+            return Multiplicators_Dict
 
 
 
     def _Get_multiplicator_of_Abroad_Comp(self, names):
-        Multiplicator_HTML = requests.get('https://finbull.ru/stock/{0}/'.format(self.__tiker), headers = {'User-Agent': ch } )
-        Multiplicators_Dict = {}
-        text = Multiplicator_HTML.text
-        for i in range(len(names)):
-            div = names[i].split("/")
-            if text.find(div[0] + " / " + div[1]) > 0:
-                Multiplicator_Value = text[text.find(div[0] + " / " + div[1]):]
-            elif text.find(div[0] + "/" + div[1]) > 0:
-                Multiplicator_Value = text[text.find(div[0] + "/" + div[1]):]
-            else:
-                Multiplicators_Dict.update({names[i]: "There is no such multiplicator"})
-                continue
-            Multiplicator_Value = Multiplicator_Value[Multiplicator_Value.find("<td"):]
-            Multiplicator_Value = Multiplicator_Value[Multiplicator_Value.find(">") + 1:Multiplicator_Value.find("</")].split()
-            Value = ""
-            for j in range(len(Multiplicator_Value)):
-                Value = Value + Multiplicator_Value[j]
-            Multiplicators_Dict.update({names[i]: Value})
+        try:
+            Multiplicator_HTML = requests.get('https://finbull.ru/stock/{0}/'.format(self.__tiker), headers = {'User-Agent': ch } )
+            Multiplicators_Dict = {}
+            text = Multiplicator_HTML.text
+            for i in range(len(names)):
+                div = names[i].split("/")
+                if text.find(div[0] + " / " + div[1]) > 0:
+                    Multiplicator_Value = text[text.find(div[0] + " / " + div[1]):]
+                elif text.find(div[0] + "/" + div[1]) > 0:
+                    Multiplicator_Value = text[text.find(div[0] + "/" + div[1]):]
+                else:
+                    Multiplicators_Dict.update({names[i]: "There is no such multiplicator"})
+                    continue
+                Multiplicator_Value = Multiplicator_Value[Multiplicator_Value.find("<td"):]
+                Multiplicator_Value = Multiplicator_Value[Multiplicator_Value.find(">") + 1:Multiplicator_Value.find("</")].split()
+                Value = ""
+                for j in range(len(Multiplicator_Value)):
+                    Value = Value + Multiplicator_Value[j]
+                Multiplicators_Dict.update({names[i]: Value})
+        except requests.exceptions.HTTPError as error:
+            print("Request error", error.response.status_code)
+            Multiplicators_Dict = {"No": 0}
+        finally:
+            return Multiplicators_Dict
 
-        return Multiplicators_Dict
 
     def Get_Company_Stocks_Grafic(self, date, Grafic_or_Arrays):
-        STOСKS_JSON = requests.get('https://api.bcs.ru/udfdatafeed/v1/history?symbol={0}&resolution=D&from={1}&to={2}'.format(self.__tiker, date[0],date[1]), headers = {'User-Agent': ch } )
-        Stoсks_Dict = json.loads(STOСKS_JSON.text)
-        if Stoсks_Dict.get("errmsg") is None:
-            Time_Array = Stoсks_Dict.get("t")
-            Time_Array = [datetime.utcfromtimestamp(e) for e in Time_Array]
-            Price_Array = Stoсks_Dict.get("c")
-        else:
-            print("There is no such tiker or date period")
-            return [0], [0]
-        if Grafic_or_Arrays == "Grafic":
+        try:
+            STOСKS_JSON = requests.get('https://api.bcs.ru/udfdatafeed/v1/history?symbol={0}&resolution=D&from={1}&to={2}'.format(self.__tiker, date[0],date[1]), headers = {'User-Agent': ch } )
+            Stoсks_Dict = json.loads(STOСKS_JSON.text)
+            if Stoсks_Dict.get("errmsg") is None:
+                Time_Array = Stoсks_Dict.get("t")
+                Time_Array = [datetime.utcfromtimestamp(e) for e in Time_Array]
+                Price_Array = Stoсks_Dict.get("c")
+            else:
+                print("There is no such tiker or date period")
+                return [0], [0]
+            if Grafic_or_Arrays == "Grafic":
+                fig = plt.figure(figsize=(20, 9))
+                plt.plot(Time_Array, Price_Array, color="coral")
+                plt.show()
+                fig.savefig(self.__tiker)
+            elif Grafic_or_Arrays == "Arrays":
+                return Time_Array, Price_Array
+            else:
+                print("This function can take only two values of Grafic_or_Arrays: Grafic or Arrays")
+        except:
+            print("Request error", STOСKS_JSON.status_code)
             fig = plt.figure(figsize=(20, 9))
-            plt.plot(Time_Array, Price_Array, color="coral")
-            plt.show()
             fig.savefig(self.__tiker)
-        elif Grafic_or_Arrays == "Arrays":
-            return Time_Array, Price_Array
-        else:
-            print("This function can take only two values of Grafic_or_Arrays: Grafic or Arrays")
-            return
+
+
+
 
     def Compare_Grafic_Of_To_Companies(self, other, date):
         selfTime_Array, selfPrice_Array = self.Get_Company_Stocks_Grafic(date, "Arrays");
@@ -136,37 +159,30 @@ class Company:
         fig.savefig(self.__tiker + "_" + other.__tiker)
 
     def parsing_divd(self):
-        r = requests.get("https://investmint.ru/{0}/".format(self.__tiker), headers={'User-Agent': ch})
-        text = r.text
-        # print(text)
-        text = text[text.find("История дивидендов") + 100:]
-        text = text[text.find("История дивидендов"):]
-        text = text[text.find("<table"):text.find("</table>") + 8]
-        # text = text[text.find("<table"):]
-        # print(text)
-        pdfkit.from_string('<html><head><meta charset="utf-8"></head><body><h1>Таблица Дивидендов {0}</h>{1}</body></html>'.format(self.__tiker, text), "Otchet_{0}.pdf".format(self.__tiker))
+        try:
+            r = requests.get("https://investmint.ru/{0}/".format(self.__tiker), headers={'User-Agent': ch})
+            text = r.text
+            # print(text)
+            text = text[text.find("История дивидендов") + 100:]
+            text = text[text.find("История дивидендов"):]
+            text = text[text.find("<table"):text.find("</table>") + 8]
+            # text = text[text.find("<table"):]
+            # print(text)
+            pdfkit.from_string('<html><head><meta charset="utf-8"></head><body><h1>Таблица Дивидендов {0}</h>{1}</body></html>'.format(self.__tiker, text), "Otchet_{0}.pdf".format(self.__tiker))
+        except requests.exceptions.HTTPError as error:
+            print("Request error", r.status_code)
+            pdfkit.from_string(
+                '<html><head><meta charset="utf-8"></head><body><h1>Таблица Дивидендов {0}</h></body></html>'.format(
+                    self.__tiker), "Otchet_{0}.pdf".format(self.__tiker))
 
 
-date = ["1600000000", "1618317179"]
 
-tiker1 = "FIVE"
-tiker2 = "T"
+#
+#
+# Comp1 = Company("RTSI", "RUS")
+# Comp2 = Company("SBER", "RUS")
+#
+# Comp1.Compare_Grafic_Of_To_Companies(Comp2, [0, 100000000000000])
 
-names = ["P/E","P/S","EV/EBITDA", "HUI/432"]
-
-Comp1 = Company(tiker1, "Russia")
-Comp2 = Company(tiker2, "USA")
-
-Comp1.Get_Company_Stocks_Grafic(date, "Grafic")
-Comp2.Get_Company_Stocks_Grafic(date, "Grafic")
-
-# Пока что это песпонтово, тк хз, как сравнивать,
-# если у одних график в баксах, а у другиз в рублях
-Comp1.Compare_Grafic_Of_To_Companies(Comp2, date)
-print(tiker1 + "   "  + str(Comp1.Get_Multiplicators(names)))
-print(tiker2 + "   "  + str(Comp2.Get_Multiplicators(names)))
-
-Comp1.parsing_divd()
-print(Comp1.get_info())
 
 
--- DataBase.py
+++ DataBase.py
@@ -1,55 +1,161 @@
 import sqlite3
+import CompaniesClass
+
 
 def DBInitialise():
-    DBConnection = sqlite3.connect("FinanceBot.db")
-    DBObject = DBConnection.cursor()
-    # Для запроса
-    # Request = DBConnection.execute()
-    DBObject.execute("""CREATE TABLE IF NOT EXISTS users(
-        userid INT PRIMARY KEY,
-        gender TEXT,
-        age TEXT); 
-    """)
-    DBConnection.commit()
-
-    DBObject.execute("""CREATE TABLE IF NOT EXISTS Companies(
-            tiker TEXT PRIMARY KEY,
-            fullname TEXT,
-            country TEXT,
-             TEXT); 
+    try:
+        DBConnection = sqlite3.connect("FinanceBot.db")
+        DBObject = DBConnection.cursor()
+        # Для запроса
+        # Request = DBConnection.execute()
+        DBObject.execute("""CREATE TABLE IF NOT EXISTS users(
+            id INT PRIMARY KEY,
+            gender TEXT,
+            age TEXT); 
         """)
+        DBConnection.commit()
 
-    DBConnection.commit()
-    DBConnection.close()
+        DBObject.execute("""CREATE TABLE IF NOT EXISTS Companies(
+                tiker TEXT PRIMARY KEY,
+                fullname TEXT,
+                country TEXT,
+                information TEXT
+                firstuser INTEGER,
+                FOREIGN KEY (firstuser) REFERENCES user(id) ON DELETE CASCADE  ON UPDATE  CASCADE); 
+            """)
+
+        DBConnection.commit()
+    except sqlite3.Error as error:
+        print("SQLite3 error", error)
+    finally:
+        if (DBConnection):
+            DBConnection.close()
 
 
 
 
-def DBAddUser(user):
-    DBConnection = sqlite3.connect("FinanceBot.db")
-    DBObject = DBConnection.cursor()
-    DBObject.execute("INSERT INTO users VALUES(?, ?, ?);", user)
-    DBConnection.commit()
-    DBConnection.close()
-
+def DBAddUser(user: tuple):
+    try:
+        DBConnection = sqlite3.connect("FinanceBot.db")
+        DBObject = DBConnection.cursor()
+        DBObject.execute("INSERT INTO users VALUES(?, ?, ?);", user)
+        DBConnection.commit()
+        DBConnection.close()
+    except sqlite3.Error as error:
+        print("SQLite3 error", error)
+    finally:
+        if (DBConnection):
+            DBConnection.close()
 
 def GetAllUsers():
-    DBConnection = sqlite3.connect("FinanceBot.db")
-    DBObject = DBConnection.cursor()
-    DBObject.execute("""SELECT * FROM users;""")
-    res = DBObject.fetchall()
-    return res
-    DBConnection.close()
+    try:
+        DBConnection = sqlite3.connect("FinanceBot.db")
+        DBObject = DBConnection.cursor()
+        DBObject.execute("""SELECT * FROM users;""")
+        res = DBObject.fetchall()
+    except sqlite3.Error as error:
+        res = []
+        print("SQLite3 error", error)
+    finally:
+        if (DBConnection):
+            DBConnection.close()
+        return res
 
 
+def GetUser(user: tuple):
+    try:
+        DBConnection = sqlite3.connect("FinanceBot.db")
+        DBObject = DBConnection.cursor()
+        DBObject.execute("""SELECT * 
+                            FROM users
+                            WHERE userid = {};""".format(user[0]))
+        res = DBObject.fetchall()
+        if res == []:
+            DBAddUser(user)
+            res = GetUser(user)
+    except sqlite3.Error as error:
+        res = []
+        print("SQLite3 error", error)
+    finally:
+        if (DBConnection):
+            DBConnection.close()
+        return res
 
-while True:
-    print("Введите Id, Пол и Возрвст пользователя через пробел")
-    UserString = input()
-    if UserString == "":
-        print("Вы ничего не ввели я отключаюсь")
-        break
-    user = tuple(UserString.split())
-    DBAddUser(user)
 
+def DBAddCompanie(Emitent: CompaniesClass.Company):
+    try:
+        DBConnection = sqlite3.connect("FinanceBot.db")
+        DBObject = DBConnection.cursor()
+        CompanyTuple=(Emitent.get_tiker(), Emitent.get_tiker(), Emitent.get_country(), Emitent.get_info())
+        DBObject.execute("INSERT INTO Companies VALUES(?, ?, ?);", CompanyTuple)
+        DBConnection.commit()
+    except sqlite3.Error as error:
+        print("SQLite3 error", error)
+    finally:
+        if (DBConnection):
+            DBConnection.close()
+
+
+def GetAllCompanies():
+    try:
+        DBConnection = sqlite3.connect("FinanceBot.db")
+        DBObject = DBConnection.cursor()
+        DBObject.execute("""SELECT * FROM Companies;""")
+        res = DBObject.fetchall()
+    except sqlite3.Error as error:
+        res = []
+        print("SQLite3 error", error)
+    finally:
+        if (DBConnection):
+            DBConnection.close()
+        return res
+
+
+def GetCompany(Emitent: CompaniesClass.Company):
+    try:
+        DBConnection = sqlite3.connect("FinanceBot.db")
+        DBObject = DBConnection.cursor()
+        DBObject.execute("""SELECT * 
+                            FROM Companies;
+                            WHERE tiker = {}""".format(Emitent.get_tiker()))
+        res = DBObject.fetchall()
+        if res == []:
+                DBAddCompanie(Emitent)
+                res = GetCompany(Emitent)
+    except sqlite3.Error as error:
+        res = []
+        print("SQLite3 error", error)
+    finally:
+        if (DBConnection):
+            DBConnection.close()
+        return res
+
+def TestAddCompanie():
+    try:
+        DBConnection = sqlite3.connect("FinanceBot.db")
+        DBObject = DBConnection.cursor()
+        CompanyTuple=tuple(tiker = "FIVE", name = "X5GDR", country = "RUSSIA", info = "paterocka perik", userid = "23")
+        DBObject.execute("INSERT INTO Companies VALUES(?, ?, ?);", CompanyTuple)
+        DBConnection.commit()
+    except sqlite3.Error as error:
+        print("SQLite3 error", error)
+    finally:
+        if (DBConnection):
+            DBConnection.close()
+
+
+DBInitialise()
+# A = CompaniesClass.Company()
+
+GetAllUsers()
+
+# while True:
+#     print("Введите Id, Пол и Возрвст пользователя через пробел")
+#     UserString = input()
+#     if UserString == "":
+#         print("Вы ничего не ввели я отключаюсь")
+#         break
+#     user = tuple(UserString.split())
+#     DBAddUser(user)
+#
 print(GetAllUsers())
